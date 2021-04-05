774110664886f1de8885d0b67b05f7fe2b21500f
ericphungg1 <80400120+ericphungg1@users.noreply.github.com>
2021-04-05 10:28:14 +1000

    Add files via upload

--- frontend-flask/webapp/app.py
+++ frontend-flask/webapp/app.py
@@ -4,11 +4,59 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('sign-in.html')
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('sign-up.html')
+
+@app.route('/register')
+def register():
+    return render_template('registration.html')
+
+@app.route('/caloriecounter')
+def calcount():
+    return render_template('calorie_counter.html')
+
+@app.route('/forum')
+def forum():
+    return render_template('community-forum.html')
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('community-leaderboards.html')
+
+@app.route('/fooddiary')
+def fooddiary():
+    return render_template('food_diary.html')
+
+@app.route('/forgotpassword')
+def forgotpassword():
+    return render_template('forgot-password.html')
+
+@app.route('/homepage')
+def homepage():
+    return render_template('home-page.html')
+
+@app.route('/personaltrainer')
+def personaltrainer():
+    return render_template('personal-trainer.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/recommendedexcercise')
+def recommendedex():
+    return render_template('recommended-exercise.html')
+
+@app.route('/recommenderecipes')
+def recommendedrec():
+    return render_template('recommended-recipes.html')
+
+@app.route('/rewards')
+def rewards():
+    return render_template('rewards.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True, host='0.0.0.0')
