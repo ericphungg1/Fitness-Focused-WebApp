52928e0728c1f2994afb630a6d16054d77d402ac
Nitharshni <nitharshni@gmail.com>
2021-04-15 21:29:01 +1000

    Update

--- seng_db.py
+++ seng_db.py
@@ -1,4 +1,7 @@
 import sqlite3,sys,os
+from datetime import datetime
+
+activeUserid = None
 
 def main():
     #create database
@@ -47,6 +50,7 @@ def main():
                                         authorid integer,
                                         title text,
                                         content text,
+                                        time text,
                                         likes text,
                                         dislikes text
                                     );"""
@@ -55,6 +59,7 @@ def main():
                                         commentid integer PRIMARY KEY,
                                         authorid integer,
                                         content text,
+                                        time text,
                                         post_id integer,
                                         FOREIGN KEY (post_id) REFERENCES posts (postid)
                                     );"""
@@ -83,6 +88,10 @@ def newuser(firstname, lastname, email, password):
         userid = 0
     else:
         userid = data[0][0] + 1
+
+    #set the active user id
+    global activeUserid
+    activeUserid = userid
 
     #enter in a new user
     action = "INSERT INTO user (userid, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)"
@@ -130,15 +139,81 @@ def login(email, password):
         return 'Email does not exist'
 
     #get the matching password of the email
-    action = "SELECT user.password from user where user.email='{}'".format(email)
+    action = "SELECT user.password, user.userid from user where user.email='{}'".format(email)
     cur.execute(action)
+    data = cur.fetchall()
 
     #check if passwords match
-    if (cur.fetchall()[0][0] == password):
+    if (data[0][0] == password):
+        global activeUserid
+        activeUserid = data[0][1]
         return 'Login successful'
     else:
         return 'Incorrect password'
     conn.close()
+
+def create_post(title, content, authorid):
+
+    #generate postid
+    conn = sqlite3.connect('smartbite.db')
+    cur = conn.cursor()
+
+    #generate user id
+    action = "SELECT MAX(postid) from posts"
+    cur.execute(action)
+    data = cur.fetchall()
+
+    ##if there are no values
+    if (data[0][0] == None):
+        postid = 0
+    else:
+        postid = data[0][0] + 1
+
+    #generate timestamp
+    now = datetime.now()
+    timestamp_string = now.strftime("%d/%m/%Y %H:%M")
+
+    #enter in a new post
+    action = "INSERT INTO posts (postid, authorid, title, content, time) VALUES (?, ?, ?, ?, ?)"
+    cur.execute(action, (postid, authorid, title, content, timestamp_string))
+
+    #print 
+    action = "SELECT * from posts where posts.authorid='{}'".format(authorid)
+    cur.execute(action)
+    print(cur.fetchall())
+
+    conn.commit()
+    conn.close()
+
+def edit_post(postid, newcontent, authorid):
+
+    conn = sqlite3.connect('smartbite.db')
+    cur = conn.cursor()
+
+    #check if the given id is same as author
+    action = "SELECT posts.authorid from posts where posts.postid={}".format(postid)
+    cur.execute(action)
+
+    postauthor = cur.fetchall()[0][0]
+    if (postauthor != authorid):
+        print("You cannot edit post")
+
+    #generate new timestamp
+    now = datetime.now()
+    timestamp_string = now.strftime("%d/%m/%Y %H:%M")
+
+    #update timestamp
+    action = "UPDATE posts SET content ='{}', time ='{}' where authorid = {}".format(newcontent, timestamp_string, authorid)
+    cur.execute(action)
+
+    #print 
+    action = "SELECT * from posts where posts.authorid='{}'".format(authorid)
+    cur.execute(action)
+    print(cur.fetchall())
+
+    conn.commit()
+    conn.close()
+
 
 def settings_updateemail(newemail, userid):
     conn = sqlite3.connect('smartbite.db')
@@ -172,6 +247,10 @@ def settings_updatepassword(newpassword, userid):
     conn.commit()
     conn.close()
 
+def logout(userid):
+    global activeUserid
+    activeUserid = None
+
 if __name__== "__main__":
     main()
     newuser('John', 'Smith', 'john@email.com', 'password')
@@ -180,4 +259,6 @@ if __name__== "__main__":
     settings_updateemail('emilyjane@email.com', 1)
     settings_updateemail('newpassword', 1)
     print(login('john@email.com', 'password'))
+    create_post('hello', 'hello', 0)
+    edit_post(0, "bye", 0)
     os.remove("smartbite.db")
